8b38457fef289f2b2a87f2b9fef25f5c826baf76
William Geddes (RIT Student) <wrg1230@ad.rit.edu>
2016-03-09 10:14:44 -0500

    Pushin the external tool

---
+++ MoonMiner - External Tool/MoonMiner - External Tool/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace MoonMiner___External_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnHard = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.brObstacleFrequency = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.brSpeed = new System.Windows.Forms.TrackBar();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.brObstacleFrequency)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(57, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Moon Miner Difficulty Modifier";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(56, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "EASY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 384);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Keep in mind: Difficulty choosen will impact your score modifier";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(279, 126);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Score Modifier: 1/2x";
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNormal.Location = new System.Drawing.Point(56, 190);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(132, 52);
+            this.btnNormal.TabIndex = 4;
+            this.btnNormal.Text = "NORMAL";
+            this.btnNormal.UseVisualStyleBackColor = true;
+            this.btnNormal.Click += new System.EventHandler(this.btnNormal_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(279, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Score Modifier: 1x";
+            // 
+            // btnHard
+            // 
+            this.btnHard.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHard.Location = new System.Drawing.Point(56, 270);
+            this.btnHard.Name = "btnHard";
+            this.btnHard.Size = new System.Drawing.Size(132, 52);
+            this.btnHard.TabIndex = 6;
+            this.btnHard.Text = "HARD";
+            this.btnHard.UseVisualStyleBackColor = true;
+            this.btnHard.Click += new System.EventHandler(this.btnHard_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(279, 288);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 20);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Score Modifier: 2x";
+            // 
+            // brObstacleFrequency
+            // 
+            this.brObstacleFrequency.Location = new System.Drawing.Point(310, 421);
+            this.brObstacleFrequency.Minimum = 1;
+            this.brObstacleFrequency.Name = "brObstacleFrequency";
+            this.brObstacleFrequency.Size = new System.Drawing.Size(104, 45);
+            this.brObstacleFrequency.TabIndex = 8;
+            this.brObstacleFrequency.Value = 1;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(193, 343);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 31);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Custom";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(52, 421);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(151, 20);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Obstacle Frequency";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(52, 473);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Speed";
+            // 
+            // brSpeed
+            // 
+            this.brSpeed.Location = new System.Drawing.Point(310, 472);
+            this.brSpeed.Minimum = 1;
+            this.brSpeed.Name = "brSpeed";
+            this.brSpeed.Size = new System.Drawing.Size(104, 45);
+            this.brSpeed.TabIndex = 12;
+            this.brSpeed.Value = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(291, 426);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(291, 478);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "1";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(420, 426);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 13);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "10";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(420, 478);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(19, 13);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "10";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(183, 514);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 35);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "CUSTOM";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 561);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.brSpeed);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.brObstacleFrequency);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnHard);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnNormal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Moon Miner Difficulty Modifier";
+            ((System.ComponentModel.ISupportInitialize)(this.brObstacleFrequency)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.brSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnNormal;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnHard;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar brObstacleFrequency;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TrackBar brSpeed;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ MoonMiner - External Tool/MoonMiner - External Tool/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MoonMiner___External_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Set score modifier
+            double scoreModifier = .5;
+
+            //Set availiability of buttons
+            button1.Enabled = false;
+            btnHard.Enabled = true;
+            btnNormal.Enabled = true;
+            button2.Enabled = true;
+
+            //Set bar values
+            brObstacleFrequency.Value = 1;
+            brSpeed.Value = 1;
+
+            //Write to file
+            string difficultyText = brObstacleFrequency.Value + " " + brSpeed.Value + " " + scoreModifier;
+            System.IO.File.WriteAllText("difficulty.txt", difficultyText);
+        }
+
+        private void btnNormal_Click(object sender, EventArgs e)
+        {
+            //Set score modifier
+            double scoreModifier = 1;
+
+            //Set availiability of buttons
+            button1.Enabled = true;
+            btnHard.Enabled = true;
+            btnNormal.Enabled = false;
+            button2.Enabled = true;
+
+            //Set bar values
+            brObstacleFrequency.Value = 5;
+            brSpeed.Value = 5;
+
+            //Write to file
+            string difficultyText = brObstacleFrequency.Value + " " + brSpeed.Value + " " + scoreModifier;
+            System.IO.File.WriteAllText("difficulty.txt", difficultyText);
+        }
+
+        private void btnHard_Click(object sender, EventArgs e)
+        {
+            //Set score modifier
+            double scoreModifier = 2;
+
+            //Set availiability of buttons
+            button1.Enabled = true;
+            btnHard.Enabled = false;
+            btnNormal.Enabled = true;
+            button2.Enabled = true;
+
+            //Set bar values
+            brObstacleFrequency.Value = 10;
+            brSpeed.Value = 10;
+
+            //Write to file
+            string difficultyText = brObstacleFrequency.Value + " " + brSpeed.Value + " " + scoreModifier;
+            System.IO.File.WriteAllText("difficulty.txt", difficultyText);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Set score modifier
+            double scoreModifier;
+            double scoreModifierFrequency = .5;
+            double scoreModifierSpeed = .5;
+            switch (brObstacleFrequency.Value)
+            {
+                case 1: scoreModifierFrequency = .5; break;
+                case 2: scoreModifierFrequency = .6; break;
+                case 3: scoreModifierFrequency = .75; break;
+                case 4: scoreModifierFrequency = .9; break;
+                case 5: scoreModifierFrequency = 1; break;
+                case 6: scoreModifierFrequency = 1.2; break;
+                case 7: scoreModifierFrequency = 1.4; break;
+                case 8: scoreModifierFrequency = 1.6; break;
+                case 9: scoreModifierFrequency = 1.8; break;
+                case 10: scoreModifierFrequency = 2; break;
+            }
+            switch (brSpeed.Value)
+            {
+                case 1: scoreModifierSpeed = .5; break;
+                case 2: scoreModifierSpeed = .6; break;
+                case 3: scoreModifierSpeed = .75; break;
+                case 4: scoreModifierSpeed = .9; break;
+                case 5: scoreModifierSpeed = 1; break;
+                case 6: scoreModifierSpeed = 1.2; break;
+                case 7: scoreModifierSpeed = 1.4; break;
+                case 8: scoreModifierSpeed = 1.6; break;
+                case 9: scoreModifierSpeed = 1.8; break;
+                case 10: scoreModifierSpeed = 2; break;
+            }
+            scoreModifier = (scoreModifierFrequency + scoreModifierSpeed) / 2;
+
+            //Set availiability of buttons
+            button1.Enabled = true;
+            btnHard.Enabled = true;
+            btnNormal.Enabled = true;
+            button2.Enabled = false;
+            brObstacleFrequency.Enabled = false;
+            brSpeed.Enabled = false;
+
+            //Write to file
+            string difficultyText = brObstacleFrequency.Value + " " + brSpeed.Value + " " + scoreModifier;
+            System.IO.File.WriteAllText("difficulty.txt", difficultyText);
+        }
+    }
+}
