0c904395dc22cfd0e3cfdd97822483f722501450
Shannon Hanley (RIT Student) <seh6271@ad.rit.edu>
2016-03-16 09:22:27 -0400

    Bare parent class for future obstacles added. Has basic attributes and necessary method signatures and properties, ready to be coded for the next milestone.

---
+++ MoonMiner/MoonMiner/Obstacles.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MoonMiner
+{
+    class Obstacles
+    {
+        //attributes
+        private Texture2D image;
+        private Rectangle pos;
+        private Boolean active;
+
+        //properties
+        public Boolean Active
+        {
+            get { return active; }
+            set { active = value; }
+        }
+
+        public Texture2D Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+        public Rectangle Pos
+        {
+            get { return pos; }
+            set { pos = value; }
+
+        }
+
+        //constructor
+        public Obstacles(Texture2D img)
+        {
+            image = img;
+            active = true;
+        }
+
+        //Movement method
+        public void Move()
+        {
+
+        }
+
+        //Collision Detection Method
+        public Boolean CheckCollision(Player plr)
+        {
+            Boolean collide = false;
+            return collide;
+        }
+
+        //draw method
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if(active == true)
+            {
+                spriteBatch.Draw(Image, pos, Color.White);
+            }
+        }
+    }
+}
