cac653b5343ac66351f28190d4e457cf9df8630e
Matthew Lefurge (RIT Student) <msl5994@ad.rit.edu>
2016-04-13 10:23:51 -0400

    Added new types of obstacles and changed game 1 class to have them spawn. Also fixed random number generation to be "more random." Changes currently in game 1, not pushed out yet. I'll probably just add it to a more up to date file.

---
+++ MoonMiner/MoonMiner/Pit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MoonMinerExecutable
+{
+    class Pit: Obstacles
+    {
+        //constructor
+        public Pit(int mod):base(mod)
+        {
+            Speed = 7;
+            Pos = new Rectangle(2000, 399, 50, 100);
+        }
+    }
+}
---
+++ MoonMiner/MoonMiner/Spike.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MoonMinerExecutable
+{
+    class Spike: Obstacles
+    {
+        public Spike(int mod):base(mod)
+        {
+            Speed = 7;
+            Pos = new Rectangle(2000, 300, 10, 100);
+        }
+    }
+}
