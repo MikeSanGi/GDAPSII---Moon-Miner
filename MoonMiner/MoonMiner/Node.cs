31ccde9478f94491beac4d9b5560d2e921c9a6d7
iDieForEXP <william.robert.geddes@gmail.com>
2016-04-27 09:52:37 -0400

    Framework for a linked list for score holding

---
+++ MoonMiner/MoonMiner/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoonMinerExecutable
+{
+    class Node
+    {
+        // Attributes \\
+        int score;
+        string name;
+        Node next;
+
+        // Constructor \\
+        public Node(string nm, int scr)
+        {
+            score = scr;
+            name = nm;
+            next = null;
+        }
+
+        // Properties \\
+        public int Score
+        {
+            get { return score; }
+            set { score = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Node Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+    }
+}
---
+++ MoonMiner/MoonMiner/ScoreLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MoonMinerExecutable
+{
+    class ScoreLinkedList
+    {
+        // Attributes \\
+        private Node head = null;
+        private int count = 0;
+
+        // Add Method \\
+        public void Add(string nm, int scr)
+        {
+            Node newNode = new Node(nm, scr);
+
+            //If nothing in the list
+            if (head == null)
+            {
+                head = newNode;
+                count++;
+                return;
+            }
+
+            //If list is already populated
+            Node current = head;
+            Node link = null;
+            while (current.Next != null)
+            {
+                if (newNode.Score < current.Score)
+                {
+                    current = current.Next;
+                }
+                if (newNode.Score >= current.Next.Score)
+                {
+                    link = current.Next;
+                    newNode = current.Next;
+                    newNode.Next = link;
+                    count++;
+                    return;
+                }
+            }
+            current.Next = newNode; // Puts this at the end of the list if it doesn't get caught
+            count++;
+        }
+    }
+}
